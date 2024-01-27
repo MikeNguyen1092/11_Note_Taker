9e1be565b80d33a20f2833088285a6dbe6aa9a0d
Mike <nguyen.mike1092@gmail.com>
2024-01-27 11:16:40 -0500

    still trying to get note to make a new .json

--- routes/index.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-
-// Import our files containing our routes
-const notesRouter = require('./notes');
-
-router.use('/notes', notesRouter);
-
-
-module.exports = router;
--- routes/notes.js
+++
@@ -1,9 +0,0 @@
-const notesRouter = require("express").Router(); // Define your route handlers
-notesRouter.get("/", (req, res) => {
-	// Logic for handling GET request to /notes
-	res.send("This is the /notes route");
-});
-
-// Additional route handlers for /notes
-
-module.exports = notesRouter;
