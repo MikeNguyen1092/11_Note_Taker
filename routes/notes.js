f0b8adfe2a052314a2631665f122848e62370fd6
Mike <nguyen.mike1092@gmail.com>
2024-01-28 12:00:15 -0500

    got note to save json

--- routes/notes.js
+++ routes/notes.js
@@ -1,12 +1,10 @@
 const notes = require("express").Router(); // Define your route handlers
 const { readFromFile, readAndAppend } = require("../helper/fsUtils");
-const uuid = require('../helpers/uuid');
+const uuid = require('../helper/uuid');
 
 
 notes.get("/", (req, res) => {
 	readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
-	// Logic for handling GET request to /notes
-	res.send("This is the /notes route");
 });
 
 notes.post("/", (req, res) => {
@@ -25,10 +23,9 @@ notes.post("/", (req, res) => {
 		readAndAppend(newNote, "./db/notes.json");
 		res.json(`Note added successfully`);
 	} else {
-		res.error("Error in adding note");
+		res.error("notes - Error in adding note");
 	}
 });
 
-// Additional route handlers for /notes
 
 module.exports = notes;
