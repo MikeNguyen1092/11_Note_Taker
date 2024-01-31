aa556fd024d1a29c27957809a5802c7c49975894
Mike <nguyen.mike1092@gmail.com>
2024-01-30 20:13:21 -0500

    able to click on the note

--- routes/notes.js
+++ routes/notes.js
@@ -8,7 +8,7 @@ notes.get("/", (req, res) => {
 });
 
 notes.post("/", (req, res) => {
-	console.info(`${req.method} request received to add a tip`);
+	console.info(`${req.method} request received to add a note`);
 	console.log(req.body);
 
 	const { title, text } = req.body;
@@ -17,10 +17,10 @@ notes.post("/", (req, res) => {
 		const newNote = {
 			title,
 			text,
-			note_id: uuid(),
+			id: uuid(),
 		};
 
-		readAndAppend(newNote, `./db/${newNote.title}.json`);
+		readAndAppend(newNote, `./db/db.json`);
 		res.json(`Note added successfully`);
 	} else {
 		res.error("notes - Error in adding note");
